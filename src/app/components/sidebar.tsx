559f4aea0a6f6e7a0e01ac3aa1bbaacd9f472a0a
Yassine Saindou <yassinesaindou12@gmail.com>
2025-05-06 09:48:52 +0530

    Initial setup

---
+++ src/app/components/sidebar.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Sidebar() {
+  return (
+    <div className="w-[300px] h-screen bg-green-500 p-4 text-gray-700 font-normal text-lg ">
+      <h1>Sidebar</h1>
+    </div>
+  );
+}
