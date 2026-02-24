054ddebac94d18a364b66179d692e042b600b249
Yassine Saindou <yassinesaindou12@gmail.com>
2026-02-24 21:22:14 +0300

    Removed the custom servive worker

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const withPWA = require('next-pwa')({
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === 'development',
-  customWorkerDir: 'worker',
+   
   clientsClaim: true,
   fallbacks: {
     document: '/offline',
